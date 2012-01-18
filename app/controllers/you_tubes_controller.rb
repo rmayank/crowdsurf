90c840437ed19d09a7450d8a0bb4f8c12e979b60
Dilip <dchouhan@systematixtechnocrates.com>
2012-01-18 17:03:18 +0530

    you tubes controller

--- app/controllers/you_tubes_controller.rb
+++ app/controllers/you_tubes_controller.rb
@@ -15,7 +15,7 @@ class YouTubesController < ApplicationController
     @upload_info = Video.token_form({"title"=>"werwerwer", "description"=>"werwer"}, save_video_new_video_url(:video_id => @video.id))
     base_url = @upload_info[:url]
     #ss = RestClient.post(base_url, :token=>@upload_info[:token], :file => File.new(Rails.root.to_s + "/public/jumps.mp4"), :content_type => 'video/mp4')
-    
+    put params
     url = URI.parse(base_url)
     req = Net::HTTP::Post::Multipart.new base_url.gsub("http://uploads.gdata.youtube.com",""),"file" => params[:userfile], "token"=>@upload_info[:token]
     res = Net::HTTP.start(url.host, url.port) do |http|
