eae6074f9e9df430806a2ba0787ae162520cced9
Dilip <dchouhan@systematixtechnocrates.com>
2012-01-18 20:40:07 +0530

    iphone call

--- app/controllers/you_tubes_controller.rb
+++ app/controllers/you_tubes_controller.rb
@@ -14,8 +14,9 @@ class YouTubesController < ApplicationController
     client = YouTubeIt::Client.new(:username => YouTubeITConfig.username, :password =>  YouTubeITConfig.password, :dev_key => YouTubeITConfig.dev_key)
     response = client.video_upload(params[:userfile].open, :title => "test",:description => "some description", :category => "People",:keywords => %w[cool blah test], :comment => "denied")
     
-    RawVideo.create(:start_time=>params[:starttimeValue].gsub("-","").to_time, :end_time=>params[:endtimeValue].gsub("-","").to_time, :latitude=>params[:latitudeValue].to_f, :longitude=>params[:longitudeValue].to_f,:video_link=>response.response.player_url)
-    render :text=>res.body
+    raw_video = RawVideo.new(:start_time=>params[:starttimeValue].gsub("-","").to_time, :end_time=>params[:endtimeValue].gsub("-","").to_time, :latitude=>params[:latitudeValue].to_f, :longitude=>params[:longitudeValue].to_f,:video_link=>response.response.player_url)
+    save_video = raw_video.save
+    render :text=>save_video
   end
   
 end
