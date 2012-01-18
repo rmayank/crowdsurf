831ec6f668cde0833d3bef32fbb5703e3c83e0aa
Dilip <dchouhan@systematixtechnocrates.com>
2012-01-18 22:20:15 +0530

    rescue video

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -5,7 +5,7 @@ module VideosHelper
   def get_watch_id(url)
     uri = URI(url)
     uri.query
-    return ( uri.query.split("&")[0].split("=")[1] rescue "")
+    return ( uri.query.split("&")[0].split("=")[1] rescue "ufhjEr-0qtw")
   end
 
 end
