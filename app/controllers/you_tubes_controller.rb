81644a4ceda0c526808df561ba78327789e01254
Dilip <dchouhan@systematixtechnocrates.com>
2012-01-27 20:20:00 +0530

    response you tube upload

--- app/controllers/you_tubes_controller.rb
+++ app/controllers/you_tubes_controller.rb
@@ -20,7 +20,11 @@ class YouTubesController < ApplicationController
       if event = ( Event.find(params[:eventId]) rescue nil)
         # need to check that input lat/long is near around events lat/long 
         event.process_videos.create(:start_time=>params[:starttimeValue].gsub("-","").to_time, :end_time=>params[:endtimeValue].gsub("-","").to_time, :latitude=>params[:latitudeValue].to_f, :longitude=>params[:longitudeValue].to_f,:video_link=>response.player_url)
-        render :text=>response
+         my_str = Array.new
+        obj.instance_variables.each do |v|
+            my_str << "#{v}: #{obj.instance_variable_get(v)}\n"
+       end
+        render :text=>my_str
      else
        CrowdSurfLog.create(:my_loger=>params.to_s)
        render :text=>"Bad Input"
