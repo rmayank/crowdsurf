c57fb321c1aaba1be92c0ea0f3fac7498c89ae83
Dilip <dchouhan@systematixtechnocrates.com>
2012-01-27 18:26:42 +0530

    you tube name changes

--- app/controllers/you_tubes_controller.rb
+++ app/controllers/you_tubes_controller.rb
@@ -13,7 +13,7 @@ class YouTubesController < ApplicationController
   def create
     begin
       client = YouTubeIt::Client.new(:username => YouTubeITConfig.username, :password =>  YouTubeITConfig.password, :dev_key => YouTubeITConfig.dev_key)
-      response = client.video_upload(params[:userfile].open, :title => "test",:description => "some description", :category => "People",:keywords => %w[cool blah test], :comment => "denied")
+      response = client.video_upload(params[:userfile].open, :title => "new title",:description => "new description", :category => "People",:keywords => %w[cool blah test], :comment => "new denied")
     
       #raw_video = RawVideo.new(:start_time=>params[:starttimeValue].gsub("-","").to_time, :end_time=>params[:endtimeValue].gsub("-","").to_time, :latitude=>params[:latitudeValue].to_f, :longitude=>params[:longitudeValue].to_f,:video_link=>response.player_url)
       #save_video = raw_video.save
