2ea5b836a04b5a1ba446ef1e872efdfa7ea82b47
Dilip <dchouhan@systematixtechnocrates.com>
2012-01-28 12:21:06 +0530

    timezone

--- app/controllers/you_tubes_controller.rb
+++ app/controllers/you_tubes_controller.rb
@@ -14,22 +14,27 @@ class YouTubesController < ApplicationController
     begin
       client = YouTubeIt::Client.new(:username => YouTubeITConfig.username, :password =>  YouTubeITConfig.password, :dev_key => YouTubeITConfig.dev_key)
       response = client.video_upload(params[:userfile].open, :title => "test",:description => "some description", :category => "People",:keywords => %w[cool blah test], :comment => "denied")
-    
-      #raw_video = RawVideo.new(:start_time=>params[:starttimeValue].gsub("-","").to_time, :end_time=>params[:endtimeValue].gsub("-","").to_time, :latitude=>params[:latitudeValue].to_f, :longitude=>params[:longitudeValue].to_f,:video_link=>response.player_url)
-      #save_video = raw_video.save
       if event = ( Event.find(params[:eventId]) rescue nil)
-        # need to check that input lat/long is near around events lat/long 
-        event.process_videos.create(:start_time=>params[:starttimeValue].gsub("-","").to_time, :end_time=>params[:endtimeValue].gsub("-","").to_time, :latitude=>params[:latitudeValue].to_f, :longitude=>params[:longitudeValue].to_f,:video_link=>response.player_url)
-         my_str = Array.new
-        response.instance_variables.each do |v|
-            my_str << "#{v}: #{response.instance_variable_get(v)}\n"
-       end
-        render :text=>my_str
+        # need to check that input lat/long is near around events lat/long
+        begin 
+          time_zone= ( params[:timeZone]  || "UTC")
+        rescue
+          time_zone = "UTC"
+        end
+        Time.zone = time_zone
+        starttime = Time.zone.parse(params[:starttimeValue].gsub("-",""))
+        starttimeValue = starttime.utc
+        endtime = Time.zone.parse(params[:endtimeValue].gsub("-",""))
+        endtimeValue = endtime.utc
+        event.process_videos.create(:start_time=>starttimeValue, :end_time=>endtimeValue, :latitude=>params[:latitudeValue].to_f, :longitude=>params[:longitudeValue].to_f,:video_link=>response.player_url, :time_zone=>time_zone)
+        Time.zone = "UTC"
+        render :text=>"Video Upload Sucessfully"
      else
        CrowdSurfLog.create(:my_loger=>params.to_s)
        render :text=>"Bad Input"
      end
    rescue
+     Time.zone = "UTC"
      CrowdSurfLog.create(:my_loger=>params.to_s)
      render :text=>"Bad Input"
    end    
