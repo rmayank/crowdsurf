54c6417993ed94aebeba96f1bd7c713f90e9991f
Dilip <dchouhan@systematixtechnocrates.com>
2012-01-28 15:24:36 +0530

    time zone exception

--- app/controllers/you_tubes_controller.rb
+++ app/controllers/you_tubes_controller.rb
@@ -16,16 +16,17 @@ class YouTubesController < ApplicationController
       response = client.video_upload(params[:userfile].open, :title => "test",:description => "some description", :category => "People",:keywords => %w[cool blah test], :comment => "denied")
       if event = ( Event.find(params[:eventId]) rescue nil)
         # need to check that input lat/long is near around events lat/long
-        begin 
-          time_zone= ( params[:timeZone]  || "UTC")
-        rescue
-          time_zone = "UTC"
-        end
+         time_zone= ( params[:timeZone]  || "UTC")
         Time.zone = time_zone
-        starttime = Time.zone.parse(params[:starttimeValue].gsub("-",""))
-        starttimeValue = starttime.utc
-        endtime = Time.zone.parse(params[:endtimeValue].gsub("-",""))
-        endtimeValue = endtime.utc
+        begin
+          starttime = Time.zone.parse(params[:starttimeValue].gsub("-",""))
+          starttimeValue = starttime.utc rescue params[:starttimeValue].gsub("-","").to_time
+          endtime = Time.zone.parse(params[:endtimeValue].gsub("-",""))
+          endtimeValue = endtime.utc rescue params[:endtimeValue].gsub("-","").to_time
+        rescue
+          starttimeValue = params[:starttimeValue].gsub("-","").to_time
+          endtimeValue = params[:endtimeValue].gsub("-","").to_time
+        end
         event.process_videos.create(:start_time=>starttimeValue, :end_time=>endtimeValue, :latitude=>params[:latitudeValue].to_f, :longitude=>params[:longitudeValue].to_f,:video_link=>response.player_url, :time_zone=>time_zone)
         Time.zone = "UTC"
         render :text=>"Video Upload Sucessfully"
